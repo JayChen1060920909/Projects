d6f3b170db9e5c492353876163e002b83d5c8b79
JayChen1060920909 <43104000+JayChen1060920909@users.noreply.github.com>
2018-10-14 09:53:26 +0800

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,15 @@
+a = int(input('(a)x:'))
+a2 = int(input('(a)x2:'))
+b = int(input('(b)x:'))
+b2 = int(input('(b)x2:'))
+c = int(input('(c)x:'))
+c2 = int(input('(c)x2:'))
+#算出a,b,c所涵蓋的範圍
+result_of_abc = a,a2,b,b2,c,c2
+result1 = max(result_of_abc)
+result2 = min(result_of_abc)
+result = result1-result2
+if(result < 0):
+    print('線段a,b,c所涵蓋的長度是',-(result))
+else:
+    print('線段a,b,c所涵蓋的長度是',result)
