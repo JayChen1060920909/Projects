b0497b01a9a0b643c74182593c72df53d53b6cdf
JayChen1060920909 <43104000+JayChen1060920909@users.noreply.github.com>
2018-10-23 18:47:20 +0800

    hw_4_算數線.py

--- hw_4.py
+++ hw_4.py
@@ -5,8 +5,5 @@ b2 = int(input('(b)x2:'))
 c = int(input('(c)x:'))
 c2 = int(input('(c)x2:'))
 #算出a,b,c所涵蓋的範圍
-result_of_abc = a,a2,b,b2,c,c2
-result1 = max(result_of_abc)
-result2 = min(result_of_abc)
-result = result1-result2
+result = c2 - a
 print(abs(result))
