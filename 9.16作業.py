60344402ed8d1fe4b1d5792f9a50ea431a3e751a
Jay Chen <43104000+JayChen1060920909@users.noreply.github.com>
2023-10-08 15:55:25 +0800

    Delete 9.16作業.py

--- 9.16作業.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import easygui
-def day(year,month,day,year2,month2,day2):     
-    day = datetime.date(year,month,day)
-    other_day = datetime.date(year2,month2,day2)
-    easygui.msgbox(day-other_day)
-easygui.msgbox(day(int(easygui.integerbox("年:",upperbound=10000,lowerbound=0)),int(easygui.integerbox("月:",upperbound=10000,lowerbound=0)),int(easygui.integerbox("日:",upperbound=10000,lowerbound=0)),int(easygui.integerbox("年:",upperbound=10000,lowerbound=0)),int(easygui.integerbox("月:",upperbound=10000,lowerbound=0)),int(easygui.integerbox("年:",upperbound=10000,lowerbound=0))))
-
