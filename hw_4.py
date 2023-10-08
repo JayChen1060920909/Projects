b00fd322844f1cfa0a0c00380506dc6e3590d36c
Jay Chen <43104000+JayChen1060920909@users.noreply.github.com>
2023-10-08 15:55:33 +0800

    Delete hw_4.py

--- hw_4.py
+++
@@ -1,9 +0,0 @@
-a = int(input('(a)x:'))
-a2 = int(input('(a)x2:'))
-b = int(input('(b)x:'))
-b2 = int(input('(b)x2:'))
-c = int(input('(c)x:'))
-c2 = int(input('(c)x2:'))
-#算出a,b,c所涵蓋的範圍
-result = c2 - a
-print(abs(result))
